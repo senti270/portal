01233b2458e9bef0befd463f6fb82884c91dd43d
cdcdcd <admin@cdcdcd.kr>
2026-01-03 20:23:30 +0900

    fix: Correct indentation for Fragment closing tag

--- app/(work-schedule)/public/payroll/[employeeId]/page.tsx
+++ app/(work-schedule)/public/payroll/[employeeId]/page.tsx
@@ -874,9 +874,9 @@ export default function PublicPayrollPage({ params }: PublicPayrollPageProps) {
               </div>
             </div>
           </div>
-              </>
-            );
-          })()}
+                </>
+              );
+            })()}
         </div>
       )}
     </div>
